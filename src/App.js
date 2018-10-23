876ef8c27ea57f341408997d98dfbc6055e0a95a
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-23 12:53:33 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-<div onClick={updateColors.bind(this)} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
-        <Tier2 handleChildClick={updateChildColors.bind(this)} color={this.state.childColor} />
-        <Tier2 handleChildClick={updateChildColors.bind(this)} color={this.state.childColor} />
-</div>
+        <Tier1 />
+        <Tier1 />
+      </div>
     )
   }
 }
